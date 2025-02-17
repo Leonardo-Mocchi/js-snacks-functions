70966dc73e20321ba2160ced6ba28137b78d40c6
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-17 15:44:05 +0100

    add es 4 DONE

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+/**
+ * Counts the vowels in a string
+ * @param {*} str your string
+ * @returns a number that represents the number of vowels in your string
+ */
+const vowelCount = str => {
+    let vowelNum = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+            vowelNum += 1
+        }
+    }
+    return vowelNum
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(vowelCount(word));
 
 
 
