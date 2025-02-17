7c18f7f952ade487dbc3e0d2b89c1ac5b506282c
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-17 15:32:00 +0100

    add es 3 DONE

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,29 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const char2FilterFor = "a"
 
 // Dichiara la funzione qui.
 
+/**
+ * Filters an array to display a second one that contains only values that begin with a character of your choice
+ * @param {*} array the array you want to filter
+ * @param {*} char your chosen character to filter for
+ * @returns an array with values that only begin with your selected character
+ */
+let filter4FirstCharacter = (array, char) => {
+    let filteredForChar = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charAt(0).toUpperCase() === char.toUpperCase()) {
+            filteredForChar.push(array[i])
+        }
+    }
+    return filteredForChar
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filter4FirstCharacter(names, char2FilterFor));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
